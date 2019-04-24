f7295aa42f94d4e6b39ad8952d091202cef93efe
sarahbrande <48233952+sarahbrande@users.noreply.github.com>
2019-04-23 22:24:58 -0600

    Add files via upload

---
+++ lect8.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+counter = 0
+while counter < 12:
+    print(counter)
+    counter = counter + 1
+
+
+# In[ ]:
+
+
+
+
+
+# In[4]:
+
+
+for i in range(5):
+    print("Hello Python")
+
+
+# In[5]:
+
+
+range(5) #different from python 2.7
+
+
+# In[6]:
+
+
+list(range(5))  # a way to see what it's doing
+
+
+# In[8]:
+
+
+for i in range(5):
+    print("Hello Python",i)
+    print("   2nd line")
+
+
+# In[9]:
+
+
+for i in range(5):
+    print("Hello Python",i)
+
+
+# In[10]:
+
+
+#anothe way
+mylist=[10,100,1000]
+
+
+# In[11]:
+
+
+mylist
+
+
+# In[16]:
+
+
+for jj in  mylist:
+    print("jj is equal to: ", jj)
+
+
+# In[ ]:
+
+
+
+
